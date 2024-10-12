1d1ae4c827f950b4e50630562ea3474d2cb459f2
Sammy930 <178405704+Sammy930@users.noreply.github.com>
2024-10-12 11:00:31 +0100

    Add some minor final changes

--- javascript.js
+++ javascript.js
@@ -4,7 +4,9 @@ let humanScore = 0, computerScore = 0;
 
 
 function getComputerChoice() {
-    // Generates a random integer between 0 and 8 and returns one of the string values (R/P/S) based on its value such that all choices have an equal probability to be returned
+    /* Generates a random integer between 0 and 8 and returns one of the string 
+    values (R/P/S) based on its value such that all choices have an equal probability 
+    to be returned */
 
 
     let prob = Math.floor(Math.random()*3);
@@ -16,14 +18,14 @@ function getComputerChoice() {
     } else {
         return S;
     };
+
 }
 
 function getHumanChoice() {
-    // Takes input from the user and returns the matching choice
+    /* Takes input from the user and returns the matching choice */
 
 
     while (true) {
-
         let choice = prompt("Enter your choice: ").toLowerCase();
 
         if (choice === "r" || choice === "rock") {
@@ -35,80 +37,87 @@ function getHumanChoice() {
         } else {
             alert("That's not a valid choice, please try again");
         };
-
     }
+
 }
 
 function playRound(humanChoice, computerChoice) {
-    // Play a round of rock paper scissors
-    // Takes both the player's and the computer's choice and Increments the humanScore or computerScore variable based on the round winner
+    /* Play a round of rock paper scissors */
+    /* Takes both the player's and the computer's choice and Increments the 
+    humanScore or computerScore variable based on the round winner */
 
 
-    let human = 0, computer = 0;    //local human and computer score variables to track the winner of each round
+    let humanScoreLocal = 0, computerScoreLocal = 0;
+    /* local score variables to track the winner of each round */
+
 
     if (humanChoice === computerChoice) {
     } else if (humanChoice === R) {
         if (computerChoice === P) {
             computerScore++;
-            computer++;
+            computerScoreLocal++;
         } else {
             humanScore++;
-            human++;
+            humanScoreLocal++;
         }
     } else if (humanChoice === P) {
         if (computerChoice === R) {
             humanScore++;
-            human++;
+            humanScoreLocal++;
         } else {
             computerScore++;
-            computer++;
+            computerScoreLocal++;
         }
     } else if (humanChoice === S) {
         if (computerChoice === R) {
             computerScore++;
-            computer++;
+            computerScoreLocal++;
         } else {
             humanScore++;
-            human++;
+            humanScoreLocal++;
         }
     }
-                
+
+    
+    /* display results */
     alert(`Player: ${humanChoice}\n---------------------------\nComputer: ${computerChoice}
         
-        ${  human > computer ? "You win this round!" :  
-            human < computer ? "Computer wins this round" : "It's a draw!"}`);     //display results
-     
+        ${humanScoreLocal > computerScoreLocal ? "You win this round!" :
+          humanScoreLocal < computerScoreLocal ? "Computer wins this round" : "It's a draw!"}`);
+        
 }
 
 function playGame() {
-    // Plays a game of rock paper scissors consisting of 5 rounds then displays the scores and the winner of the game
+    /* Plays a game of rock paper scissors consisting of 5 rounds then displays
+    the scores and the winner of the game */
 
 
-    let play = true;    // playing condition 
+    let play = true;    /* playing condition */ 
 
     while (play === true) {
 
+        /* Play 5 rounds */
         let i = 0;
         while (i < 5) {
             let humanchoice = getHumanChoice();
-            let computerchoice = getComputerChoice();
-            
+            let computerchoice = getComputerChoice();            
             playRound(humanchoice, computerchoice);
-
             i++;
         }
 
 
+        /* Display results */
         alert(`The score is: 
             Player: ${humanScore} | Computer: ${computerScore}
             
             ${  humanScore > computerScore ? "Player wins!!" :  
-                humanScore < computerScore ? "Computer wins :(" : "Draw"}`);    //Display results
+                humanScore < computerScore ? "Computer wins :(" : "Draw"}`);    
 
 
+        /* Ask the player if they want to play another round and change the 
+        playing condition based on their input */
         while (true) {
-
-        let p = prompt("Play again? (Y/N)").toLowerCase();     //Ask the player if they want to play another round and change the playing condition based on their input
+        let p = prompt("Play again? (Y/N)").toLowerCase();
         if (p === "n" || p === "no") {
             play = false;
             break;
@@ -119,10 +128,10 @@ function playGame() {
         } else {
             alert("invalid input");
         };
-
     };
 
     }
+
 }
 
 
@@ -133,4 +142,5 @@ or simply (r/p/s)
     
 Enjoy :)`);
 
-playGame();
+
+playGame();     /* Game starts */
